92e831a721fc148b377153985b7582fa1adaa165
Mohammad Ali <ali.mohammed200@gmail.com>
2018-11-20 08:41:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`
   }
 }
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+
+  if (right < 100) {
+    dodger.style.right = `${right + 1}px`
+  }
+}
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
+  }
+  else if(e.which === 39){
+    moveDodgerRight();
   }
 })
